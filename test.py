91ee52c0b311e58057e60a4951dd07b0c8f2fb94
wau2380(yijung park) <79182893+wau2380@users.noreply.github.com>
2022-07-20 11:56:31 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,5 @@
-
-
+#소수찾기 자기자신과 1을 약수로 가지는 수 
 n = int(input())
-now_pd = int(input())
-future_pd = int(input())
-
-now_pd1 = list(map(int, str(now_pd)))
-future_pd1 = list(map(int, str(future_pd)))
-
-cnt = 0; i = 0; 
-print(now_pd1)
-while now_pd1 != future_pd1:
-    if(now_pd1[i] == 0):
-        now_pd1[i] = 10
-    if(future_pd1[i] == 0):
-        future_pd1[i] = 10 
-      
-    if (now_pd1[i] > future_pd1[i]): #67890
-        now_pd1[i]-=1
-        cnt+=1
-    
-    elif (now_pd1[i] < future_pd1[i]):
-        now_pd1[i]+=1
-        cnt+=1
-    i+=1
-print(cnt)
-
-
-
-
+numbers = map(int, input())
+numbers = list(numbers)
+print(numbers, type(numbers))
